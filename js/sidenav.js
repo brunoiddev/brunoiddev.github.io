4c6b9b59a7a884eb7a2ec0444e1a0e3417f09c04
brunoid <brunocanongia@gmail.com>
2017-09-08 12:10:45 -0300

    sidenav fecha clicando fora

--- js/sidenav.js
+++ js/sidenav.js
@@ -2,6 +2,7 @@ function openNav() {
 	if (document.getElementById("mySidenav").offsetWidth =="0") {
 		document.getElementById("mySidenav").style.width = "250px";
 		document.getElementById("seta").style.webkitTransform = 'rotate(-90deg)';
+		document.getElementById("content").style.opacity = 1.5;
 	} else {
 		document.getElementById("mySidenav").style.width = "0";
 		document.getElementById("seta").style.webkitTransform = 'rotate(0deg)';
@@ -11,4 +12,13 @@ function openNav() {
 
 function closeNav() {
 	document.getElementById("mySidenav").style.width = "0";
+}
+
+window.onload = function() {
+    document.getElementById("content").onclick = function() {
+        if (document.getElementById("mySidenav").offsetWidth =="250") {
+        	document.getElementById("mySidenav").style.width = "0";
+			document.getElementById("seta").style.webkitTransform = 'rotate(0deg)';
+    	}
+    }
 }
